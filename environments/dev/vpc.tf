eb73c283cbc96a7bb1b79502bd666e91fcc077ce
Geoffrey <glouasse@gmail.com>
2025-03-02 21:54:49 +0100

    :sparkles: feat: fixed vars and added router

--- environments/dev/vpc.tf
+++ environments/dev/vpc.tf
@@ -4,6 +4,7 @@
 
 /*
   TODO: remove and replace creation / deletion of google_vpc_access_connector in workflows to avoid costs
+  TODO: remove and replace creation / deletion of nat and router in workflows to avoid costs
 */
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -65,4 +66,25 @@ resource "google_vpc_access_connector" "serverless_connector" {
   machine_type  = "e2-micro"
   min_instances = 2
   max_instances = 3
+}
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# 🟢 Serverless VPC connector
+# ----------------------------------------------------------------------------------------------------------------------
+
+resource "google_compute_router" "datastats_router" {
+  name    = "${var.project_name}-router"
+  project = var.project_id
+  region  = var.region
+  network = google_compute_network.datastats_network.id
+}
+
+resource "google_compute_router_nat" "datastats_nat" {
+  name                               = "${var.project_name}-nat"
+  project                            = var.project_id
+  router                             = google_compute_router.datastats_router.name
+  region                             = var.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
 }
