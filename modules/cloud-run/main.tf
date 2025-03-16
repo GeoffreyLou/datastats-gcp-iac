0ea56b2708e1ad99fc9ec6bdd549e59a1e4848e8
Geoffrey <glouasse@gmail.com>
2025-03-16 19:48:18 +0100

    :bug: fix: fixed local exec for cloud run

--- modules/cloud-run/main.tf
+++ modules/cloud-run/main.tf
@@ -97,13 +97,13 @@ resource "null_resource" "deploy_sample_job" {
 
   provisioner "local-exec" {
     command = <<EOT
-      docker build -t ${var.region}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.main.name}/${var.job_name}:latest ../../modules/cloud-run/sample/.
+      docker build -t "${var.region}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.main.name}/${var.job_name}:latest" ../../modules/cloud-run/sample/.
     EOT
   }
 
   provisioner "local-exec" {
     command = <<EOT
-      docker push ${var.region}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.main.name}/${var.job_name}:latest
+      docker push "${var.region}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.main.name}/${var.job_name}:latest"
     EOT
   }
 }
